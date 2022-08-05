5fe4c76556056b4ae8771cff35b0063ebf336aff
miss-f-g <fatemeghafari98@gmail.com>
2022-08-05 15:41:17 +0430

    7.add anim to page 1+2

---
+++ app.js
@@ -0,0 +1,39 @@
+//Pin the first page
+const tlIntro = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".first-page",
+    start: "0%",
+    end: "100%",
+    pin: true,
+    pinSpacing: false,
+  },
+});
+
+//Highlight PAGE 2
+const tlH = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    markers: { startColor: "blue", endColor: "blue" },
+    scrub: true,
+    start: "-37%",
+    end: "40%",
+  },
+});
+
+tlH.fromTo(
+  ".highlight",
+  { color: "rgba(255,255,255, 0.4" },
+  { color: "rgba(255,255,255, 1", stagger: 1 }
+);
+
+const tlHRemove = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second-page",
+    markers: { startColor: "pink", endColor: "pink" },
+    scrub: true,
+    start: "-20%",
+    end: "60%",
+  },
+});
+
+tlHRemove.to(".highlight", { color: "rgba(255,255,255, 0.4", stagger: 1 });
